c0df91a857ff2730e900287e21e5462f47c76d3d
Your Name <you@example.com>
2020-03-03 10:04:23 -0700

    Continued work on node spotify formatting, calls artist info with correct formating.  Started work on bands in town node calling.  All nodules installed and initialized.

--- LIRI.js
+++ LIRI.js
@@ -1,10 +1,11 @@
 require("dotenv").config();
 
 //node module imports needed for functions
-//var request = require("request");
+var request = require("request");
 var Spotify = require('node-spotify-api');
 var keys = require("./keys.js");
 var fs = require("fs");
+
 // var used for terminal command if/elses
 var liriArg = process.argv[2];
 
@@ -13,12 +14,108 @@ var liriArg = process.argv[2];
     song();
 } else if (liriArg === "movie-this") {
     movie();
-} else if (liriArg === "do-what-it-says") {
-    doSome();
+} else if (liriArg === "concert-this") {
+    concert();
 } else {
-    console.log("Please enter one of the following commands: spotify-this-song, movie-this, do-what-it-says.");
+    console.log("Please enter one of the following commands: spotify-this-song, movie-this, concert-this.");
 }
+//BANDS IN TOWN
 
+function concert(){
+
+    if ('concert-this')
+    {
+        var artist="";
+        for (var i = 3; i < process.argv.length; i++)
+        {
+            artist+=process.argv[i];
+        }
+    let bandsFig = "Bandsintown"
+        figlet(bandsFig, function(err, data) {
+            if (err) {
+                console.log('Something went wrong...');
+                console.dir(err);
+                return;
+            }
+            console.log(chalk.green(data));
+        });
+        // console.log(artist);
+    }
+    else
+    {
+        artist = parameter;
+    }
+    
+    var queryUrl = "https://rest.bandsintown.com/artists/"+ artist +"/events?app_id=codingbootcamp";
+    
+    
+    request(queryUrl, function(error, response, body) {
+    
+      if (!error && response.statusCode === 200) {
+    
+        var JS = JSON.parse(body);
+        for (i = 0; i < JS.length; i++)
+        {
+          var dateTime = JS[i].datetime;
+            var month = dateTime.substring(5,7);
+            var year = dateTime.substring(0,4);
+            var day = dateTime.substring(8,10);
+            var dateForm = month + "/" + day + "/" + year
+      
+          display(chalk.blue("\n---------------------------------------------------\n"));
+          display(chalk.green("Name: " + JS[i].venue.name));
+          display(chalk.green("City: " + JS[i].venue.city));
+          if (JS[i].venue.region !== "")
+          {
+            display(chalk.green("Country: " + JS[i].venue.region));
+          }
+          display(chalk.green("Country: " + JS[i].venue.country));
+          display(chalk.green("Date: " + dateForm));
+          display(chalk.blue("\n---------------------------------------------------\n"));
+    
+        }
+      }
+    });
+    }
+
+//function to show movie data
+function movie() {
+
+    var args = process.argv;
+    var movieName = "";
+
+    for (i = 3; i < args.length; i++) {
+        if (i > 3 && i < args.length) {
+            movieName = movieName + "+" + args[i];
+        } else {
+            movieName = args[i];
+        }
+    };
+
+    if (movieName === "") {
+        movieName = "Forrest" + "+" + "Gump"
+    };
+
+    //run a request to the OMDB API with the specified movie
+    var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+
+    request(queryUrl, function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+            console.log("-------------------------------------------------------------------------------------------");
+            console.log("Title: " + JSON.parse(body).Title);
+            console.log("Year: " + JSON.parse(body).Year);
+            console.log("IMDB rating: " + JSON.parse(body).imdbRating);
+            console.log("Rotten Tomatoes rating: " + JSON.parse(body).Ratings[1].Value);
+            console.log("Country: " + JSON.parse(body).Country);
+            console.log("Language: " + JSON.parse(body).Language);
+            console.log("Plot: " + JSON.parse(body).Plot);
+            console.log("Actors: " + JSON.parse(body).Actors);
+            console.log("-------------------------------------------------------------------------------------------");
+        } else {
+            console.log("ya' messed up");
+        }
+    });
+};
 
 //function to show spotify data
 function song() {
@@ -59,7 +156,7 @@ function song() {
 };
 
 //random fs function
-function doSome() {
+function concert() {
 
     fs.readFile("random.txt", "utf8", function (error, data) {
         if (error) {
